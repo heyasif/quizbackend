07f7ca0a36458378c4c19bbe31055c0cd9ec3137
Asif <heyasif@asif.local>
2024-03-30 19:26:51 +0530

    dsd

--- routes/user.route.js
+++ routes/user.route.js
@@ -88,6 +88,20 @@ userRouter.delete("/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+// Endpoint to update a user
+userRouter.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { username, email, role } = req.body;
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(id, { username, email, role }, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User successfully updated", updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 module.exports = { userRouter };
